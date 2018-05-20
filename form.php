3f46695ad6f1223b8bd324c2c3a8aab6c7561e72
aqilhoshmand <aqil.hoshmand@gmail.com>
2018-05-21 04:20:11 +0430

    creat project

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+	<form>
+			 First Name:<br><br><input type="text" name="Firstname">
+			Last Name:<br><br><input type="text" name="lastname">
+			Email Address:<br><br><input type="text" name="email">
+			<input type="submit" name="send" value="send">
+
+
+
+	</form>
+</body>
+</html>
